60dd210644d56400688b1a2e944b1d716a2dbfe5
Stefan Bucur <stefan@dslab-stefan-workstation.epfl.ch>
2010-05-03 23:27:33 +0200

    Reformatted the LBConnection class.

--- lib/Cloud9/Worker/LBConnection.cpp
+++ lib/Cloud9/Worker/LBConnection.cpp
@@ -24,278 +24,277 @@ namespace cloud9 {
 
 namespace worker {
 
-LBConnection::LBConnection(boost::asio::io_service &s,
-		JobManager *jm)
-	: service(s), socket(s), jobManager(jm), id(0) {
-
+LBConnection::LBConnection(boost::asio::io_service &s, JobManager *jm) :
+  service(s), socket(s), jobManager(jm), id(0) {
 
 }
 
 void LBConnection::connect(boost::system::error_code &error) {
-	connectSocket(service, socket, LBAddress, LBPort, error);
-
+  connectSocket(service, socket, LBAddress, LBPort, error);
 
 }
 
 LBConnection::~LBConnection() {
-	socket.close();
+  socket.close();
 }
 
 void LBConnection::updateLogPrefix() {
-	char prefix[] = "Worker<   >: ";
-	sprintf(prefix, "Worker<%03d>: ", id);
+  char prefix[] = "Worker<   >: ";
+  sprintf(prefix, "Worker<%03d>: ", id);
 
-	cloud9::Logger::getLogger().setLogPrefix(prefix);
+  cloud9::Logger::getLogger().setLogPrefix(prefix);
 }
 
 void LBConnection::registerWorker() {
-	// Prepare the registration message
-	WorkerReportMessage message;
-	message.set_id(0); // Don't have an ID yet
+  // Prepare the registration message
+  WorkerReportMessage message;
+  message.set_id(0); // Don't have an ID yet
 
-	WorkerReportMessage_Registration *reg = message.mutable_registration();
-	reg->set_address(LocalAddress);
-	reg->set_port(LocalPort);
-	reg->set_wants_updates(UseGlobalCoverage);
+  WorkerReportMessage_Registration *reg = message.mutable_registration();
+  reg->set_address(LocalAddress);
+  reg->set_port(LocalPort);
+  reg->set_wants_updates(UseGlobalCoverage);
 
-	llvm::sys::Path progPath(InputFile);
+  llvm::sys::Path progPath(InputFile);
 
-	reg->set_prog_name(progPath.getBasename());
-	reg->set_stat_id_count(jobManager->getCoverageIDCount());
-	reg->set_prog_crc(jobManager->getModuleCRC());
+  reg->set_prog_name(progPath.getBasename());
+  reg->set_stat_id_count(jobManager->getCoverageIDCount());
+  reg->set_prog_crc(jobManager->getModuleCRC());
 
-	// Send the message
-	std::string msgString;
-	bool result = message.SerializeToString(&msgString);
-	assert(result);
+  // Send the message
+  std::string msgString;
+  bool result = message.SerializeToString(&msgString);
+  assert(result);
 
-	sendMessage(socket, msgString);
+  sendMessage(socket, msgString);
 
-	std::string respString;
-	// Wait for the response
-	recvMessage(socket, respString);
+  std::string respString;
+  // Wait for the response
+  recvMessage(socket, respString);
 
-	LBResponseMessage response;
-	result = response.ParseFromString(respString);
-	assert(result);
+  LBResponseMessage response;
+  result = response.ParseFromString(respString);
+  assert(result);
 
-	id = response.id();
+  id = response.id();
 
-	updateLogPrefix();
+  updateLogPrefix();
 
-	CLOUD9_INFO("Worker registered with the load balancer - ID: " << id);
+  CLOUD9_INFO("Worker registered with the load balancer - ID: " << id);
 
-	processResponse(response);
+  processResponse(response);
 }
 
 void LBConnection::sendJobStatistics(WorkerReportMessage &message) {
-	WorkerReportMessage_NodeDataUpdate *dataUpdate = message.mutable_nodedataupdate();
-	std::vector<int> data;
-	int total = 0;
-	ExecutionPathSetPin paths = ExecutionPathSet::getEmptySet();
+  WorkerReportMessage_NodeDataUpdate *dataUpdate =
+      message.mutable_nodedataupdate();
+  std::vector<int> data;
+  int total = 0;
+  ExecutionPathSetPin paths = ExecutionPathSet::getEmptySet();
 
-	jobManager->getStatisticsData(data, paths, true);
+  jobManager->getStatisticsData(data, paths, true);
 
-	//CLOUD9_DEBUG("Sending " << data.size() << " update values and " <<
-	//		paths.size() << " update nodes");
+  //CLOUD9_DEBUG("Sending " << data.size() << " update values and " <<
+  //		paths.size() << " update nodes");
 
-	for (std::vector<int>::iterator it = data.begin(); it != data.end(); it++) {
-		dataUpdate->add_data(*it);
-		total += *it;
-	}
+  for (std::vector<int>::iterator it = data.begin(); it != data.end(); it++) {
+    dataUpdate->add_data(*it);
+    total += *it;
+  }
 
-	CLOUD9_DEBUG("Reporting " << total << " jobs to the load balancer");
+  CLOUD9_DEBUG("Reporting " << total << " jobs to the load balancer");
 
-	if (paths->count() > 0 || data.size() == 0) {
-		assert(paths->count() == data.size());
+  if (paths->count() > 0 || data.size() == 0) {
+    assert(paths->count() == data.size());
 
-		WorkerReportMessage_NodeSetUpdate *setUpdate = message.mutable_nodesetupdate();
-		cloud9::data::ExecutionPathSet *pathSet = setUpdate->mutable_pathset();
+    WorkerReportMessage_NodeSetUpdate *setUpdate =
+        message.mutable_nodesetupdate();
+    cloud9::data::ExecutionPathSet *pathSet = setUpdate->mutable_pathset();
 
-		serializeExecutionPathSet(paths, *pathSet);
-	}
+    serializeExecutionPathSet(paths, *pathSet);
+  }
 }
 
 void LBConnection::sendCoverageUpdates(WorkerReportMessage &message) {
-	cov_update_t data;
-	jobManager->getUpdatedLocalCoverage(data);
+  cov_update_t data;
+  jobManager->getUpdatedLocalCoverage(data);
 
-	if (data.size() > 0) {
-		CLOUD9_DEBUG("Sending " << data.size() << " local coverage updates.");
+  if (data.size() > 0) {
+    CLOUD9_DEBUG("Sending " << data.size() << " local coverage updates.");
 
-		StatisticUpdate *update = message.add_localupdates();
-		serializeStatisticUpdate(CLOUD9_STAT_NAME_LOCAL_COVERAGE, data, *update);
-	}
+    StatisticUpdate *update = message.add_localupdates();
+    serializeStatisticUpdate(CLOUD9_STAT_NAME_LOCAL_COVERAGE, data, *update);
+  }
 }
 
 void LBConnection::sendStrategyUpdates(WorkerReportMessage &message) {
-	StrategyPortfolio *portfolio =
-			dynamic_cast<StrategyPortfolio*>(jobManager->getStrategy());
+  StrategyPortfolio *portfolio =
+      dynamic_cast<StrategyPortfolio*> (jobManager->getStrategy());
 
-	if (portfolio == NULL) {
-		// The manager does not use a strategy portfolio, just drop these updates
-		return;
-	}
+  if (portfolio == NULL) {
+    // The manager does not use a strategy portfolio, just drop these updates
+    return;
+  }
 
-	WorkerReportMessage_StrategyPortfolioUpdate *update =
-					message.mutable_strategyportfolioupdate();
+  WorkerReportMessage_StrategyPortfolioUpdate *update =
+      message.mutable_strategyportfolioupdate();
 
-	for (std::vector<unsigned int>::const_iterator it = portfolio->getStrategies().begin();
-			it != portfolio->getStrategies().end(); it++) {
-		unsigned int strat = *it;
-		StrategyPortfolioData *data = update->add_data();
-		data->set_strategy(strat);
-		data->set_allocation(portfolio->getStrategyAllocation(strat));
-		data->set_performance(portfolio->getStrategyPerformance(strat));
-	}
+  for (std::vector<unsigned int>::const_iterator it =
+      portfolio->getStrategies().begin(); it
+      != portfolio->getStrategies().end(); it++) {
+    unsigned int strat = *it;
+    StrategyPortfolioData *data = update->add_data();
+    data->set_strategy(strat);
+    data->set_allocation(portfolio->getStrategyAllocation(strat));
+    data->set_performance(portfolio->getStrategyPerformance(strat));
+  }
 }
 
 void LBConnection::sendUpdates() {
-	// Prepare the updates message
-	WorkerReportMessage message;
-	message.set_id(id);
+  // Prepare the updates message
+  WorkerReportMessage message;
+  message.set_id(id);
 
-	sendJobStatistics(message);
+  sendJobStatistics(message);
 
-	sendCoverageUpdates(message);
+  sendCoverageUpdates(message);
 
-	std::string msgString;
-	bool result = message.SerializeToString(&msgString);
-	assert(result);
-	sendMessage(socket, msgString);
+  std::string msgString;
+  bool result = message.SerializeToString(&msgString);
+  assert(result);
+  sendMessage(socket, msgString);
 
-	std::string respString;
-	recvMessage(socket, respString);
+  std::string respString;
+  recvMessage(socket, respString);
 
-	LBResponseMessage response;
-	result = response.ParseFromString(respString);
-	assert(result);
+  LBResponseMessage response;
+  result = response.ParseFromString(respString);
+  assert(result);
 
-	processResponse(response);
+  processResponse(response);
 }
 
 void LBConnection::processResponse(LBResponseMessage &response) {
-	assert(id == response.id());
+  assert(id == response.id());
 
-	if (response.more_details()) {
-		jobManager->setRefineStatistics();
-	}
+  if (response.more_details()) {
+    jobManager->setRefineStatistics();
+  }
 
-	if (response.has_jobtransfer()) {
-		CLOUD9_DEBUG("Job transfer request");
+  if (response.has_jobtransfer()) {
+    CLOUD9_DEBUG("Job transfer request");
 
-		const LBResponseMessage_JobTransfer &transDetails =
-				response.jobtransfer();
+    const LBResponseMessage_JobTransfer &transDetails = response.jobtransfer();
 
-		std::string destAddress = transDetails.dest_address();
-		int destPort = transDetails.dest_port();
+    std::string destAddress = transDetails.dest_address();
+    int destPort = transDetails.dest_port();
 
-		ExecutionPathSetPin paths;
-		std::vector<int> counts;
+    ExecutionPathSetPin paths;
+    std::vector<int> counts;
 
-		paths = parseExecutionPathSet(transDetails.path_set());
+    paths = parseExecutionPathSet(transDetails.path_set());
 
-		counts.insert(counts.begin(), transDetails.count().begin(),
-				transDetails.count().end());
+    counts.insert(counts.begin(), transDetails.count().begin(),
+        transDetails.count().end());
 
-		transferJobs(destAddress, destPort, paths, counts);
-	}
+    transferJobs(destAddress, destPort, paths, counts);
+  }
 
-	if (response.has_jobseed()) {
-		const LBResponseMessage_JobSeed &seedDetails =
-				response.jobseed();
+  if (response.has_jobseed()) {
+    const LBResponseMessage_JobSeed &seedDetails = response.jobseed();
 
-		const cloud9::data::ExecutionPathSet &pathSet = seedDetails.path_set();
-		ExecutionPathSetPin paths;
+    const cloud9::data::ExecutionPathSet &pathSet = seedDetails.path_set();
+    ExecutionPathSetPin paths;
 
-		paths = parseExecutionPathSet(pathSet);
+    paths = parseExecutionPathSet(pathSet);
 
-		CLOUD9_DEBUG("Job seed request: " << paths->count() << " paths");
+    CLOUD9_DEBUG("Job seed request: " << paths->count() << " paths");
 
-		if (seedDetails.strategies_size() > 0) {
-			std::vector<unsigned int> strategies;
+    if (seedDetails.strategies_size() > 0) {
+      std::vector<unsigned int> strategies;
 
-			for (int i = 0; i < seedDetails.strategies_size(); i++)
-				strategies.push_back(seedDetails.strategies(i));
+      for (int i = 0; i < seedDetails.strategies_size(); i++)
+        strategies.push_back(seedDetails.strategies(i));
 
-			jobManager->importJobs(paths, &strategies);
-		} else {
-			jobManager->importJobs(paths, NULL);
-		}
-	}
+      jobManager->importJobs(paths, &strategies);
+    } else {
+      jobManager->importJobs(paths, NULL);
+    }
+  }
 
-	if (UseGlobalCoverage) {
-		for (int i = 0; i < response.globalupdates_size(); i++) {
-			const StatisticUpdate &update = response.globalupdates(i);
+  if (UseGlobalCoverage) {
+    for (int i = 0; i < response.globalupdates_size(); i++) {
+      const StatisticUpdate &update = response.globalupdates(i);
 
-			if (update.name() == CLOUD9_STAT_NAME_GLOBAL_COVERAGE) {
-				cov_update_t data;
+      if (update.name() == CLOUD9_STAT_NAME_GLOBAL_COVERAGE) {
+        cov_update_t data;
 
-				parseStatisticUpdate(update, data);
+        parseStatisticUpdate(update, data);
 
-				if (data.size() > 0) {
-					CLOUD9_INFO("Receiving " << data.size() << " global coverage updates.");
-					jobManager->setUpdatedGlobalCoverage(data);
-				}
-			}
-		}
-	}
+        if (data.size() > 0) {
+          CLOUD9_INFO("Receiving " << data.size() << " global coverage updates.");
+          jobManager->setUpdatedGlobalCoverage(data);
+        }
+      }
+    }
+  }
 
-	if (response.strategyportfolioresponse_size() > 0) {
-		for (int i = 0; i < response.strategyportfolioresponse_size(); i++) {
-			const StrategyPortfolioResponse &stratResp = response.strategyportfolioresponse(i);
+  if (response.strategyportfolioresponse_size() > 0) {
+    for (int i = 0; i < response.strategyportfolioresponse_size(); i++) {
+      const StrategyPortfolioResponse &stratResp =
+          response.strategyportfolioresponse(i);
 
-			reInvestJobs(stratResp.newstrategy(), stratResp.oldstrategy(),
-					stratResp.nrjobs());
-		}
-	}
+      reInvestJobs(stratResp.newstrategy(), stratResp.oldstrategy(),
+          stratResp.nrjobs());
+    }
+  }
 }
 
 void LBConnection::transferJobs(std::string &destAddr, int destPort,
-		ExecutionPathSetPin paths,
-		std::vector<int> counts) {
+    ExecutionPathSetPin paths, std::vector<int> counts) {
 
-	std::vector<unsigned int> strategies;
-	ExecutionPathSetPin jobPaths = jobManager->exportJobs(paths, counts, &strategies);
+  std::vector<unsigned int> strategies;
+  ExecutionPathSetPin jobPaths = jobManager->exportJobs(paths, counts,
+      &strategies);
 
-	tcp::socket peerSocket(service);
-	boost::system::error_code error;
+  tcp::socket peerSocket(service);
+  boost::system::error_code error;
 
-	connectSocket(service, peerSocket, destAddr, destPort, error);
+  connectSocket(service, peerSocket, destAddr, destPort, error);
 
-	if (error) {
-		CLOUD9_ERROR("Could not connect to the peer worker");
-		return;
-	}
+  if (error) {
+    CLOUD9_ERROR("Could not connect to the peer worker");
+    return;
+  }
 
-	PeerTransferMessage message;
-	cloud9::data::ExecutionPathSet *pSet = message.mutable_path_set();
+  PeerTransferMessage message;
+  cloud9::data::ExecutionPathSet *pSet = message.mutable_path_set();
 
-	serializeExecutionPathSet(jobPaths, *pSet);
+  serializeExecutionPathSet(jobPaths, *pSet);
 
-	for (unsigned int i = 0; i < strategies.size(); i++) {
-		message.add_strategies(strategies[i]);
-	}
+  for (unsigned int i = 0; i < strategies.size(); i++) {
+    message.add_strategies(strategies[i]);
+  }
 
-	std::string msgString;
-	message.SerializeToString(&msgString);
+  std::string msgString;
+  message.SerializeToString(&msgString);
 
-	sendMessage(peerSocket, msgString);
+  sendMessage(peerSocket, msgString);
 
-	peerSocket.close();
+  peerSocket.close();
 }
 
 void LBConnection::reInvestJobs(unsigned int oldStrat, unsigned int newStrat,
-		unsigned int maxCount) {
-	StrategyPortfolio *portfolio =
-			dynamic_cast<StrategyPortfolio*>(jobManager->getStrategy());
+    unsigned int maxCount) {
+  StrategyPortfolio *portfolio =
+      dynamic_cast<StrategyPortfolio*> (jobManager->getStrategy());
 
-	if (portfolio == NULL) {
-		CLOUD9_INFO("Cannot reinvest jobs as no strategy portfolio is used.");
-		return;
-	}
+  if (portfolio == NULL) {
+    CLOUD9_INFO("Cannot reinvest jobs as no strategy portfolio is used.");
+    return;
+  }
 
-	portfolio->reInvestJobs(newStrat, oldStrat, maxCount);
+  portfolio->reInvestJobs(newStrat, oldStrat, maxCount);
 }
 
 }
