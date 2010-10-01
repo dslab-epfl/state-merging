5929383b9c77d86f6434b976fbd59cc6ce9b0006
Stefan Bucur <stefan.bucur@epfl.ch>
2010-10-02 01:30:37 +0200

    Updated the user space header.

--- include/user/symbolics.h
+++ include/user/symbolics.h
@@ -17,6 +17,7 @@ extern "C" {
 void klee_make_symbolic(void *addr, size_t nbytes, const char *name) __attribute__((weak));
 
 #define KLEE_SIO_SYMREADS   0xfff00     // Enable symbolic reads for a socket
+#define KLEE_SIO_READCAP    0xfff01     // Set a maximum cap for reading from the stream
 
 #ifdef  __cplusplus
 }
