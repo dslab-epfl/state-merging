f1f2b5465b272218979683a12d727f791b4aa765
Vlad Ureche <sun@dslabpc9.epfl.ch>
2010-10-07 22:09:41 +0200

    Some tweaks to for curl

--- include/user/symbolics.h
+++ include/user/symbolics.h
@@ -9,6 +9,7 @@
 #define STUBS_H_
 
 #include <stdio.h>
+#include <stdint.h>
 
 #ifdef  __cplusplus
 extern "C" {
@@ -16,6 +17,7 @@ extern "C" {
 
 void klee_make_symbolic(void *addr, size_t nbytes, const char *name) __attribute__((weak));
 void klee_event(unsigned int type, long int value) __attribute__((weak));
+void klee_assume(uintptr_t condition) __attribute__((weak));
 
 #define KLEE_SIO_SYMREADS   0xfff00     // Enable symbolic reads for a socket
 #define KLEE_SIO_READCAP    0xfff01     // Set a maximum cap for reading from the stream
