920c0d5dc0e9d92c706b916c36246417252e37dc
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2011-03-15 12:04:28 +0100

    Revert "Refactored init_env() function to allocate all symbolic arrays before doing any forking"
    This reverts commit 43a935416996028dcf590bcfa00d481f5307ba3f.

--- runtime/POSIX/klee_init_env.c
+++ runtime/POSIX/klee_init_env.c
@@ -72,31 +72,31 @@ static char *__get_sym_str(int numChars, char *name) {
   return s;
 }
 
+static void __add_arg(int *argc, char **argv, char *arg, int argcMax) {
+  if (*argc==argcMax) {
+    __emit_error("too many arguments for klee_init_env");
+  } else {
+    argv[*argc] = arg;
+    (*argc)++;
+  }
+}
+
 void klee_init_env(int* argcPtr, char*** argvPtr) {
   int argc = *argcPtr;
   char** argv = *argvPtr;
 
+  int new_argc = 0, n_args;
+  char* new_argv[1024];
+  char* tmp_argv[1024];
+  unsigned max_len, min_argvs, max_argvs;
   unsigned sym_files = 0, sym_file_len = 0;
   char unsafe_flag = 0;
-
-  int new_argc = 0;
-  int new_argc_max = 0;
-  char* new_argv[1024];
-
-  int new_argv_rcount = 0;
-  int new_argv_r[2048];
-
-  int final_argc = 0;
   char** final_argv;
-
-  unsigned max_len, min_argvs, max_argvs;
   char sym_arg_name[5] = "arg";
-  char sym_narg_name[10] = "nargs";
   unsigned sym_arg_num = 0;
   int k=0, i;
 
   sym_arg_name[4] = '\0';
-  sym_narg_name[6] = '\0';
 
 #if 0
   // Recognize --help when it is the sole argument.
@@ -118,54 +118,43 @@ usage: (klee_init_env) [options] [program arguments]\n\
     if (__streq(argv[k], "--sym-arg") || __streq(argv[k], "-sym-arg")) {
       const char *msg = "--sym-arg expects an integer argument <max-len>";
       if (++k == argc)        
-        __emit_error(msg);
-
+	__emit_error(msg);
+		
       max_len = __str_to_int(argv[k++], msg);
       sym_arg_name[3] = '0' + sym_arg_num++;
-
-      if (new_argc_max==1024)
-        __emit_error("too many arguments for klee_init_env");
-
-      new_argv[new_argc_max++] = __get_sym_str(max_len, sym_arg_name);
+      __add_arg(&new_argc, new_argv, 
+                __get_sym_str(max_len, sym_arg_name),
+                1024);
     }
     else if (__streq(argv[k], "--sym-args") || __streq(argv[k], "-sym-args")) {
       const char *msg = 
         "--sym-args expects three integer arguments <min-argvs> <max-argvs> <max-len>";
 
       if (k+3 >= argc)
-        __emit_error(msg);
+	__emit_error(msg);
       
       k++;
       min_argvs = __str_to_int(argv[k++], msg);
       max_argvs = __str_to_int(argv[k++], msg);
       max_len = __str_to_int(argv[k++], msg);
 
-      if (new_argc_max+max_argvs > 1024)
-        __emit_error("too many arguments for klee_init_env");
-
-      if (min_argvs < max_argvs) {
-        new_argv_r[new_argv_rcount*2  ] = new_argc_max + min_argvs;
-        new_argv_r[new_argv_rcount*2+1] = new_argc_max + max_argvs;
-        ++new_argv_rcount;
-      }
-
       for (i=0; (unsigned) i < max_argvs; ++i) {
         sym_arg_name[3] = '0' + sym_arg_num++;
-        new_argv[new_argc_max++] = __get_sym_str(max_len, sym_arg_name);
+        tmp_argv[i] = __get_sym_str(max_len, sym_arg_name);
       }
 
       //n_args = klee_range(min_argvs, max_argvs+1, "n_args");
-      //klee_make_symbolic(&n_args, sizeof(n_args), "n_args");
-      //klee_assume(n_args >= (int) min_argvs && n_args <= (int) max_argvs);
-      //for (i=0; i < n_args; i++) {
-      //  __add_arg(&new_argc, new_argv, tmp_argv[i], 1024);
-      //}
+      klee_make_symbolic(&n_args, sizeof(n_args), "n_args");
+      klee_assume(n_args >= (int) min_argvs && n_args <= (int) max_argvs);
+      for (i=0; i < n_args; i++) {
+        __add_arg(&new_argc, new_argv, tmp_argv[i], 1024);
+      }
     }
     else if (__streq(argv[k], "--sym-files") || __streq(argv[k], "-sym-files")) {
       const char* msg = "--sym-files expects two integer arguments <no-sym-files> <sym-file-len>";      
 
       if (k+2 >= argc)
-        __emit_error(msg);
+	__emit_error(msg);
       
       k++;
       sym_files = __str_to_int(argv[k++], msg);
@@ -178,39 +167,23 @@ usage: (klee_init_env) [options] [program arguments]\n\
     }
     else {
       /* simply copy arguments */
-      if (new_argc_max==1024)
-        __emit_error("too many arguments for klee_init_env");
-      new_argv[new_argc_max++] = argv[k++];
+      __add_arg(&new_argc, new_argv, argv[k++], 1024);
     }
   }
 
+  final_argv = (char**) malloc((new_argc+1) * sizeof(*final_argv));
+  klee_mark_global(final_argv);
+  memcpy(final_argv, new_argv, new_argc * sizeof(*final_argv));
+  final_argv[new_argc] = 0;
+
+  *argcPtr = new_argc;
+  *argvPtr = final_argv;
+
   klee_init_processes();
-  klee_init_mmap();
 
   klee_init_fds(sym_files, sym_file_len, unsafe_flag);
 
-  final_argv = (char**) malloc((new_argc_max+1) * sizeof(*final_argv));
-  klee_mark_global(final_argv);
-
-  for (i=0; i < new_argv_rcount; ++i) {
-    sym_narg_name[5] = '0' + i;
-    new_argv_r[i*2] = klee_range(new_argv_r[i*2], new_argv_r[i*2+1]+1, sym_narg_name);
-  }
-
-  for (i=0; i < new_argv_rcount; ++i) {
-    // And now we fork...
-    for (; new_argc < new_argv_r[i*2];)
-      final_argv[final_argc++] = new_argv[new_argc++];
-    new_argc = new_argv_r[i*2+1];
-  }
-
-  for (; new_argc < new_argc_max;)
-    final_argv[final_argc++] = new_argv[new_argc++];
-
-  final_argv[final_argc] = 0;
-
-  *argcPtr = final_argc;
-  *argvPtr = final_argv;
+  klee_init_mmap();
 
   klee_event(__KLEE_EVENT_BREAKPOINT, __KLEE_BREAK_TRACE);
 }
