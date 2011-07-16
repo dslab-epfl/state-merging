775b71e87134927786564ef853267078ad174eff
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2011-07-16 19:55:26 +0200

    Parse options in analysis/data.py

--- scripts/analysis/data.py
+++ scripts/analysis/data.py
@@ -23,9 +23,11 @@ class Experiment(object):
         self.run_stats_file = None
         self.open_run_stats_file()
         self.update_run_stats_file()
+        self.update_info_file()
 
     def update(self):
         self.update_run_stats_file()
+        self.update_info_file()
 
     def open_run_stats_file(self):
         """ Open run.stats file and read it's header """
@@ -74,6 +76,48 @@ class Experiment(object):
         self.a = ClassDict()
         for k, v in self.stats.iteritems():
             self.a[k] = numpy.array(v, dtype=numpy.double)
+
+    def update_info_file(self):
+        if hasattr(self, 'info_file') and self.info_file is not None:
+            self.info_file.close()
+
+        info_file_fname = os.path.join(self.klee_out_dir, 'info')
+        try:
+            self.info_file = open(info_file_fname, 'r')
+        except IOError:
+            sys.errlog.write('Can not open %s\n' % info_file_fname)
+            self.info_file = None
+            return
+
+        self.cmdline = self.info_file.readline()
+
+        self.opts = ClassDict()
+        for opt in self.cmdline.split(' ')[1:]:
+            if not opt.startswith('-'):
+                break
+            if opt.startswith('--'):
+                opt = opt[2:]
+            elif opt.startswith('-'):
+                opt = opt[1:]
+            
+            opt = opt.split('=')
+            optname = opt[0].replace('-', '_')
+            if len(opt) == 1:
+                self.opts[optname] = '1'
+            else:
+                self.opts[optname] = opt[1]
+
+        self.is_done = False
+        self.total_instructions = 0
+        self.completed_paths = 0
+
+        for line in self.info_file:
+            if line.startswith('Finished:'):
+                self.is_done = True
+            elif line.startswith('KLEE: done: total instructions ='):
+                self.total_instructions = int(line.split('=')[1])
+            elif line.startswith('KLEE: done: completed paths ='):
+                self.completed_paths = int(line.split('=')[1])
 
     def __getitem__(self, key):
         return self.stats[key]
